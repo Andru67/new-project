2a7d4179cf76c07344c6750a1d419d4d74f38ce4
Andru <andru8140@gmail.com>
2023-10-31 22:00:42 +0200

    swiper about done 31.10.23

--- src/components/Main/About/AboutHeader/AboutHeaderNav.jsx
+++ src/components/Main/About/AboutHeader/AboutHeaderNav.jsx
@@ -5,38 +5,39 @@ import cn from "classnames";
 
 
 
-export const AboutHeaderNav = ({}) => {
+export const AboutHeaderNav = ({activeSlide, isBeginning, isEnd}) => {     //  принимаем пропс
+
+    console.log(activeSlide)           // проверка передачи пропсов
+    console.log(isBeginning)
+    console.log(isEnd)
 
 
     const swiper = useSwiper();
-
-   /* const SlideTo = ({slide, isActive}) => {
-        const swiper = useSwiper();
-        const handleClick = () => swiper.slideTo(slide)
-
-        if (isActive) return <button><b>0{slide + 1}</b></button>
-         return <button onClick={handleClick}>SlideTo {slide + 1}</button>
-    }
-    const [activeSlide, setActiveSlide] = useState(0)
-    console.log(activeSlide)
-*/
+    // console.log(swiper)
 
     return (
         <div className={st.about_header_space_buttons}>
             <div className={st.about_header_space_buttons_bloc}>
-                <div className={st.about_header_space_button_prev}>
-                    <div onClick={() => swiper.slidePrev()}>PREV</div>
+                <div className={st.about_header_space_buttons_toggle}>
+                    <button
+                        onClick={() => swiper.slidePrev()}
+                        className={cn(st.btn_prev, {[st.disable]: isBeginning})}
+                        disabled={isBeginning}
+                    >PREV</button>
                     <div>|</div>
-                    <div onClick={() => swiper.slideNext()}>NEXT</div>
+                    <button
+                        onClick={() => swiper.slideNext()}
+                        className={cn(st.btn_next, {[st.disable]: isEnd})}
+                        disabled={isEnd}
+                    >NEXT</button>
                 </div>
             </div>
             <div className={cn(st.about_header_space_buttons_bloc, st.back)}>
                 <div
-                    className={st.about_header_space_buttons_bloc_num}
-                    // tabIndex={0}
+                    className={cn(st.about_header_space_buttons_bloc_num, {[st.active]: activeSlide === 0})}
                      onClick={() => swiper.slideTo(0)}
-                >01
-                    {/*<SlideTo slide={0} isActive={activeSlide === 0 }/>*/}
+                >
+                    01
                 </div>
                 <div className={st.about_header_space_buttons_bloc_text}>
                     <div>Lorem Ipsum</div>
@@ -46,11 +47,10 @@ export const AboutHeaderNav = ({}) => {
             </div>
             <div className={cn(st.about_header_space_buttons_bloc, st.back)}>
                 <div
-                    className={st.about_header_space_buttons_bloc_num}
-                    // tabIndex={0}
+                    className={cn(st.about_header_space_buttons_bloc_num, {[st.active]: activeSlide === 1})}
                     onClick={() => swiper.slideTo(1)}
-                >02
-                    {/*<SlideTo slide={1} isActive={activeSlide === 1 }/>*/}
+                >
+                    02
                 </div>
                 <div className={st.about_header_space_buttons_bloc_text}>
                     <div>Lorem Ipsum</div>
@@ -59,10 +59,10 @@ export const AboutHeaderNav = ({}) => {
             </div>
             <div className={cn(st.about_header_space_buttons_bloc, st.back)}>
                 <div
-                    className={st.about_header_space_buttons_bloc_num}
-                    // tabIndex={0}
+                    className={cn(st.about_header_space_buttons_bloc_num, {[st.active]: activeSlide === 2})}
                     onClick={() => swiper.slideTo(2)}
-                >03
+                >
+                    03
                 </div>
                 <div className={st.about_header_space_buttons_bloc_text}>
                     <div>Lorem Ipsum</div>
--- src/components/Main/About/AboutHeader/SlicerAboutHeader/SlicerAboutHeader.jsx
+++ src/components/Main/About/AboutHeader/SlicerAboutHeader/SlicerAboutHeader.jsx
@@ -13,9 +13,20 @@ import {AboutHeaderNav} from "../AboutHeaderNav";
 
 export const SlicerAboutHeader = ({}) => {
 
+    const [activeSlide, setActiveSlide] = useState(0)
+    const [isBeginning, setIsBeginning] = useState(true)
+    const [isEnd, setIsEnd] = useState(false)
+
+    const handleSlideChange = swiper => {        // *  берем нужные состояния   стр.45
+        setActiveSlide(swiper.activeIndex);
+        setIsBeginning(swiper.isBeginning);
+        setIsEnd(swiper.isEnd);
+    };
+
+
 
     return (
-        <Swiper
+        <Swiper onSlideChange={handleSlideChange}
             slidesPerView={1}
             spaceBetween={1}
             /*keyboard={{
@@ -28,7 +39,8 @@ export const SlicerAboutHeader = ({}) => {
             //modules={[Keyboard, Pagination, Navigation]}
             className="mySwiper"
         >
-            <AboutHeaderNav />
+
+            <AboutHeaderNav activeSlide={activeSlide} isBeginning={isBeginning} isEnd={isEnd}/> {/* стр.45 прокодываем пропс*/}
             <SwiperSlide><AboutTitle_01/></SwiperSlide>
             <SwiperSlide><AboutTitle_02/></SwiperSlide>
             <SwiperSlide><AboutTitle_03/></SwiperSlide>
